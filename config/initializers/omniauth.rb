5c824c1e2f61696cf1d52042213aa10032a3fdb1
Danielle <danielle@daniellemullane.com>
2015-04-18 16:39:47 -0600

    Add flash message div in application.html.erb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,3 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
-
-# provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-#          :scope => 'email', :display => 'popup'
