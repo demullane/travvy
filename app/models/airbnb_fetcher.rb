e2e8038b29abb88bc5ffa7725feb50202d09733d
Danielle <danielle@daniellemullane.com>
2015-05-20 18:53:52 -0600

    Make edits to airbnb to match hotel style and vice-versa, show more hotel details on hotel page

--- app/models/airbnb_fetcher.rb
+++ app/models/airbnb_fetcher.rb
@@ -17,31 +17,31 @@ class AirbnbFetcher
 
   def airbnb_pretty_results #(location, checkin, checkout, guests)
     self.search_airbnb #(location, checkin, checkout, guests)
-    @titles = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-name']}
-    @links = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-url']}
-    @prices = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-price']}
-    @imgs = @page.search("//span[contains(concat(' ', @class, ' '),' wish_list_button ')]").collect {|node| node['data-img']}
-    @addresses = @page.search("//span[contains(concat(' ', @class, ' '),' wish_list_button ')]").collect {|node| node['data-address']}
-    @latitudes = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-lat']}
-    @longitudes = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-lng']}
+    titles = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-name']}
+    links = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-url']}
+    prices = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-price']}
+    imgs = @page.search("//span[contains(concat(' ', @class, ' '),' wish_list_button ')]").collect {|node| node['data-img']}
+    location_description = @page.search("//span[contains(concat(' ', @class, ' '),' wish_list_button ')]").collect {|node| node['data-address']}
+    latitudes = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-lat']}
+    longitudes = @page.search("//div[contains(concat(' ', @class, ' '),' listing ')]").collect {|node| node['data-lng']}
 
-    @links = @links.map do |link|
+    links = links.map do |link|
       add_on = "https://www.airbnb.com"
       add_on << link
       link = add_on
     end
 
-    @prices = @prices.map do |price|
+    prices = prices.map do |price|
       number = price.scan(/\d/).join('')
       price = "$" + number
     end
 
-    @airbnb_results = []
-    @titles.each_with_index do |val, index|
-      @airbnb_results << {:title => val.split.map(&:capitalize).join(' '), :link => @links[index], :price => @prices[index], :image => @imgs[index], :address => @addresses[index], :latitude => @latitudes[index], :longitude => @longitudes[index]}
+    airbnb_results = []
+    titles.each_with_index do |val, index|
+      airbnb_results << {:title => val.split.map(&:capitalize).join(' '), :link => links[index], :price => prices[index], :image => imgs[index], :location_description => location_description[index], :latitude => latitudes[index], :longitude => longitudes[index]}
     end
 
-    return @airbnb_results
+    return airbnb_results
   end
 
 end
--- app/models/hotel_fetcher.rb
+++ app/models/hotel_fetcher.rb
@@ -11,25 +11,58 @@ class HotelFetcher
   def search_hotels
     response = @hotel_search.get do |req|
       req.params['apiKey'] = ENV['EXPEDIA_KEY']
-      req.params['cid'] = 55505 # ENV['CID']- testing
+      req.params['cid'] = 55505
       req.params['minorRev'] = 4
       req.params['locale'] = 'en_US'
       req.params['currencyCode'] = 'USD'
-      req.params['arrivalDate'] = '05/21/2015'
-      req.params['departureDate'] = '05/22/2015'
+      req.params['arrivalDate'] = '05/21/2015' #checkin variable
+      req.params['departureDate'] = '05/22/2015' #checkout variable
       req.params['numberOfResults'] = 18
-      req.params['room1'] = 2
-      req.params['city'] = 'Denver'
-      req.params['stateProvinceCode'] = 'CO'
-      req.params['countryCode'] = 'US'
+      req.params['room1'] = 2 #adult count variable
+      req.params['city'] = 'Denver' #city variable
+      req.params['stateProvinceCode'] = 'CO' #state variable
+      req.params['countryCode'] = 'US' #country variable
     end
     JSON.parse(response.body)
   end
 
   def hotel_pretty_results
     data = self.search_hotels
-    @hotels = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['name']}
-    return @hotels
+    titles = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['name']}
+    hotel_ids = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['hotelId']}
+    prices = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['RoomRateDetailsList']['RoomRateDetails']['RateInfo']['ChargeableRateInfo']['NightlyRatesPerRoom']['NightlyRate']['@rate']}
+    # imgs =
+    location_descriptions = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['locationDescription']}
+    latitudes = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['latitude']}
+    longitudes = data['HotelListResponse']['HotelList']['HotelSummary'].map{ |hotel| hotel['longitude']}
+
+    links = []
+    hotel_ids.each do |id|
+      url = []
+      url[0] = 'http://www.travelnow.com/templates/336616/hotels/' + id.to_s + '/overview?'
+      url[1] = 'lang=en&currency=USD'
+      url[2] = '&secureUrlFromDataBridge=https%3A%2F%2Fwww.travelnow.com'
+      url[3] = '&requestVersion=V2'
+      url[4] = '&checkin=05%2F21%2F2015' #checkin variable
+      url[5] = '&checkout=05%2F22%2F2015' #checkout variable
+      url[6] = '&creditCardInfo.needStartDate=true'
+      url[7] = '&destination=Denver%2C+CO%2C+United+States' #city, state, country variables
+      url[8] = '&filter.highPrice=2147483647'
+      url[9] = '&filter.lowPrice=0'
+      url[10] = '&roomsCount=1' #room count variable
+      url[11] = '&rooms[0].adultsCount=2' #adult count variable
+      url[12] = '&rooms[0].childrenCount=0' #child count variable?
+      url[13] = '&standardCheckin=5%2F21%2F2015' #checkin variable
+      url[14] = '&standardCheckout=5%2F22%2F2015' #checkout variable
+      links << url.join('')
+    end
+
+    hotel_results = []
+    titles.each_with_index do |val, index|
+      hotel_results << {:title => val, :link => links[index], :price => prices[index], :location_description => location_descriptions[index], :latitude => latitudes[index], :longitude => longitudes[index]}
+    end
+
+    return hotel_results
   end
 
 end
